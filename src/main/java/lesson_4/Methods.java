18e8feb40d128c7ab8c0b3eccc32ed6c361b4f9f
nataliarabo4aya <nrabo4aya@gmail.com>
2022-10-29 20:32:33 +0300

    -homeWork lesson4

---
+++ src/main/java/lesson_4/Methods.java
@@ -0,0 +1,34 @@
+package lesson_4;
+
+import java.util.Random;
+
+public class Methods {
+    private void publicMethod() {
+        System.out.println("Public Method");
+    }
+    public static void division (int num1, int num2){
+        try {
+            int result = num1 / num2;
+            System.out.println("Результат деления " +result);
+        } catch (ArithmeticException error) {
+            System.out.println("Деление на ноль недопустимая операция" + error.getMessage());
+        }
+    }
+    public static void adding (int num1, int num2){
+        int result = num1 + num2;
+        System.out.println("Результат сложения " +result);
+    }
+    public static void substraction (int num1, int num2){
+        int result = num1 - num2;
+        System.out.println("Результат вичетания " +result);
+    }
+    public static void multiplying (int num1, int num2){
+        int result = num1 * num2;
+        System.out.println("Результат умножения " +result);
+    }
+    public static void persentage (double num1, double num2){
+        double result = (num1 * num2)/100;
+        System.out.println("Процент " +result);
+    }
+
+}
