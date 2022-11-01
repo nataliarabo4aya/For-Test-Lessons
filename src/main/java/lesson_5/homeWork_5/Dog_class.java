bbf980ec91f86e1d5ff3b838db4f87208a9dcaaf
nataliarabo4aya <nrabo4aya@gmail.com>
2022-11-01 21:08:20 +0200

    -homeWork_5

---
+++ src/main/java/lesson_5/homeWork_5/Dog_class.java
@@ -0,0 +1,70 @@
+package lesson_5.homeWork_5;
+
+public class Dog_class {
+    private String nameDog;
+    private int ageDog;
+    private String colorDog;
+    private int weightDog;
+
+
+
+    //конструктор с четирьмя параметрами
+    public Dog_class(String nameDog, int ageDog, String colorDog, int weightDog) {
+        this.nameDog = nameDog;
+        this.ageDog = ageDog;
+        this.colorDog = colorDog;
+        this.weightDog = weightDog;
+    }
+//конструктор со значениями по умолчанию
+
+    public Dog_class(int ageDog, String colorDog) {
+        this.nameDog = nameDog;
+        this.weightDog = weightDog;
+        this.ageDog = 3;
+        this.colorDog = "Black";
+    }
+
+    //констуктор которий не принимает никакие параметри
+
+    public Dog_class() {
+
+    }
+    public void printInfo(){
+        System.out.println("Creating object of the class Dog.");
+    }
+
+    public String getNameDog() {
+        return nameDog;
+    }
+
+    public int getAgeDog() {
+        return ageDog;
+    }
+
+    public String getColorDog() {
+        return colorDog;
+    }
+
+    public int getWeightDog() {
+        return weightDog;
+    }
+
+    public void setNameDog(String nameDog) {
+        this.nameDog = nameDog;
+    }
+
+    public void setAgeDog(int ageDog) {
+        if (ageDog >0 && ageDog < 25) {
+            this.ageDog = ageDog;
+        }
+    }
+
+    public void setColorDog(String colorDog) {
+        this.colorDog = colorDog;
+    }
+
+    public void setWeightDog(int weightDog) {
+        this.weightDog = weightDog;
+    }
+}
+
---
+++ src/main/java/lesson_5/homeWork_5/Work.java
@@ -0,0 +1,22 @@
+package lesson_5.homeWork_5;
+
+public class Work {
+    public static void main(String[] args) {
+        Dog_class dog = new Dog_class("Cookie", 2, "White", 5);
+        Dog_class dogShort = new Dog_class(5, "Gray");
+        Dog_class dogEmtyFields = new Dog_class();
+
+        System.out.println(dog.getColorDog());
+        System.out.println(dog.getWeightDog());
+        System.out.println(dog.getNameDog());
+        System.out.println(dogShort.getAgeDog());
+        dog.setWeightDog(4);
+        System.out.println(dog.getWeightDog());
+        dog.setAgeDog(26);
+        System.out.println(dog.getAgeDog());
+        System.out.println(dogShort.getColorDog());
+        dogEmtyFields.printInfo();
+
+
+    }
+}
