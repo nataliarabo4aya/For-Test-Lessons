b389587f52095b1912979ffdbd863e348f62795a
nataliarabo4aya <nrabo4aya@gmail.com>
2022-11-21 14:42:56 +0200

    -homeWork_6_update

--- src/main/java/homeWork6/objects6/Cat6.java
+++ src/main/java/homeWork6/objects6/Cat6.java
@@ -3,9 +3,7 @@ package homeWork6.objects6;
 public class Cat6 extends Animal6 {
     private String color;
 
-    public Cat6(String color) {
-        this.color = "Spotted";
-    }
+
 //геттери и сеттери
     public String getColor() {
         return color;
@@ -14,10 +12,15 @@ public class Cat6 extends Animal6 {
     public void setColor(String color) {
         this.color = color;
     }
-
+//конструктори
+    //1-ий принимает все парамтери:
     public Cat6(boolean vegetarian, String eats, int noOfLegs, String color) {
         super(vegetarian, eats, noOfLegs);
         this.color = color;
     }
 
+    public Cat6(boolean vegetarian, String eats, int noOfLegs) {
+        super(vegetarian, eats, noOfLegs);
+        this.color = "Spotted";
+    }
 }
